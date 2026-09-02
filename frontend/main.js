b8e02142ec1181af14284ce74cd4eab7513b89e1
Ujjwal200706 <ujjwal3901@gmail.com>
2026-09-02 07:42:36 -0700

    frontend

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@
 // CONFIGURATION
 // =====================================================
 
-const BACKEND_URL = "http://127.0.0.1:5000";
+const BACKEND_URL = "https://sandeep-enterprises.onrender.com";
 
 // =====================================================
 // DOM ELEMENTS
